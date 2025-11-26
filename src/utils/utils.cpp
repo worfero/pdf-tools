a739acabb987fa21ac868e78e67634c3c123f788
Henrique Wolf <henriquewolf1999@gmail.com>
2025-11-26 09:48:50 -0300

    Revert "better trailer parsing"
    This reverts commit a43bebd5b95b513bb384ea6753fcabf9257f9f46.

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -30,10 +30,7 @@ std::map<std::string, std::string> parse_dict(std::string dict_buffer){
     std::string token, key, val;
     while(!dict_stream.eof()){
         std::getline(dict_stream, token, ' ');
-        if(token[0] != '/'){
-            key = "/";
-        }
-        key += token;
+        key = "/" + token;
 
         std::getline(dict_stream, token, '/');
         while(!token.empty() && std::isspace(static_cast<unsigned char>(token.back()))){
